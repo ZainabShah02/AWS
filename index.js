f2ef0315c44d44dc4a3f50153a0d5e2a190278c7
Jacky <you@example.com>
2023-05-05 15:43:30 +0500

    Changed Popo is my babez to Tommy

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+<<<<<<< Updated upstream
   res.send('Popo is my babez')
+=======
+  res.send('Tommy')
+>>>>>>> Stashed changes
 })
 
 app.listen(port, () => {
